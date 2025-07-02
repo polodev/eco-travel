c6ad09c818b24f6eb1ffa06f2477de8cdfa60df3
polodev <polodev10@gmail.com>
2025-07-02 16:59:05 +0600

    updated with admin-dashboard-route

--- app-modules/admin-dashboard/routes/admin-dashboard-routes.php
+++ app-modules/admin-dashboard/routes/admin-dashboard-routes.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\AdminDashboard\Http\Controllers\DashboardController;
 use Modules\AdminDashboard\Http\Controllers\UserController;
 
-Route::middleware(['auth', 'role.access:developer,admin,employee,accounts'])->group(function () {
+Route::middleware(['web', 'auth', 'role.access:developer,admin,employee,accounts'])->group(function () {
     Route::prefix('admin')->name('admin-dashboard.')->group(function () {
-        Route::get('/', [DashboardController::class, 'index'])->name('index');
+        Route::get('/', function () {
+            return view('admin-dashboard::index');
+        })->name('index');
         
         Route::resource('users', UserController::class);
         Route::get('users-json', [UserController::class, 'indexJson'])->name('users.json');
