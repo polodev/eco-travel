9ce359ddf003b2d58f220d36970d6bcced2e07c7
polodev <polodev10@gmail.com>
2025-07-28 13:08:43 +0600

    updated with old code

--- config/mail.php
+++ config/mail.php
@@ -51,6 +51,9 @@ return [
 
         'ses' => [
             'transport' => 'ses',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_SES_REGION', env('AWS_DEFAULT_REGION', 'us-east-1')),
         ],
 
         'postmark' => [
