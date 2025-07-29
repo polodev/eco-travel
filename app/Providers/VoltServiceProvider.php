2ac4c9adc64eaebb83b26b7ec4cdcbbd9e027565
polodev <polodev10@gmail.com>
2025-07-29 14:36:36 +0600

    updated volt service provider for user data

--- app/Providers/VoltServiceProvider.php
+++ app/Providers/VoltServiceProvider.php
@@ -23,6 +23,7 @@ class VoltServiceProvider extends ServiceProvider
         Volt::mount([
             config('livewire.view_path', resource_path('views/livewire')),
             resource_path('views/pages'),
+            base_path('app-modules/user-data/resources/views/livewire'),
             base_path('app-modules/option/resources/views/livewire'),
         ]);
     }
