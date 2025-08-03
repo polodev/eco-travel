eefd749af949e3570a827746b34437cb946bccd1
polodev <polodev10@gmail.com>
2025-08-03 23:10:38 +0600

    added static stie module

---
+++ app-modules/static-site/routes/static-site-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use Modules\StaticSite\Http\Controllers\StaticSiteController;
+
+// Route::get('/static-sites', [StaticSiteController::class, 'index'])->name('static-sites.index');
+// Route::get('/static-sites/create', [StaticSiteController::class, 'create'])->name('static-sites.create');
+// Route::post('/static-sites', [StaticSiteController::class, 'store'])->name('static-sites.store');
+// Route::get('/static-sites/{static-site}', [StaticSiteController::class, 'show'])->name('static-sites.show');
+// Route::get('/static-sites/{static-site}/edit', [StaticSiteController::class, 'edit'])->name('static-sites.edit');
+// Route::put('/static-sites/{static-site}', [StaticSiteController::class, 'update'])->name('static-sites.update');
+// Route::delete('/static-sites/{static-site}', [StaticSiteController::class, 'destroy'])->name('static-sites.destroy');
